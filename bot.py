4caf0156b29e418d84da40581b211c1b0cb84631
Getta <16712480+BasedJumper@users.noreply.github.com>
2021-01-11 12:22:28 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,49 +1,24 @@
-from discord.ext import commands 
-import discord
-import asyncio
 import robin_stocks as r
 import pyotp
 
-
-# dont look at this 
+#info from  robinhood
 totp = pyotp.TOTP("your key").now()
 login = r.login('username','password', mfa_code=totp)
-
-#prefix for the bot 
-bot = commands.Bot(command_prefix='!')
 ask_price=[]
 bid_price=[]
+crypto="DOGE"
+startingprice= float(r.get_crypto_quote(crypto,"ask_price"))
+startingamountmoney =float(r.load_account_profile("cash"))
+dogeamout=r.get_crypto_positions("quantity")
+starrtamontofcrypto =float(dogeamout[0])
+buyamount=1
+sellamount=1
+
+def buy(crypto,amountofcrypto,price):
+	r.order_buy_crypto_limit(crypto,amountofcrypto,price)
+
+def sell(crypto,orderamount,priceofcrypto):
+	r.order_sell_crypto_limit(crypto, orderamount, priceofcrypto, timeInForce='gtc')
 
 
 
-
-
-
-
-
-
-
-
-#pulls crypto info form RH
-def getcrypto(pdata):
-	historical_quotes =r.get_crypto_quote(pdata)
-	return historical_quotes
-
-
-#  bot is ready
-@bot.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
-#when a message is sent keep a log of it, then await for a command
-@bot.event
-async def on_message(message):
-	print("Message:", message.content)
-	await bot.process_commands(message)
-
-#list of command functions 
-@bot.command()
-async def clear(ctx):
-	await ctx.message.channel.purge()
-
-
-bot.run('discord bot token')
